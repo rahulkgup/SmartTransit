9c3cf18b5eca36d44bed94b8d9eb97301dd6fa66
Rahul Gupta <rahulkgup@gmail.com>
2025-09-26 20:25:45 -0400

    Initial Commit

---
+++ SmartTransit/SmartTransitApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartTransitApp.swift
+//  SmartTransit
+//
+//  Created by Rahul Gupta on 9/26/25.
+//
+
+import SwiftUI
+
+@main
+struct SmartTransitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
